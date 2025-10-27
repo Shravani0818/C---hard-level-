fdac0d6a60427823c1e73980eefe2b834ae84ce9
Shravani0818 <shravaniwaghmare2006@gmail.com>
2025-10-27 08:05:28 -0700

    Add files via upload

---
+++ first_15_even_num.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main() {
+    for(int i = 2; i <= 30; i += 2) printf("%d ", i);
+    return 0;
+}
---
+++ num_50_1.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main() {
+    for(int i = 50; i >= 1; i--) printf("%d ", i);
+    return 0;
+}
